f61b8a8dedb3c0e5c773f51fcfb079a9a0cc66ae
duminicaalexandru <129775424+dumyalex01@users.noreply.github.com>
2023-09-20 16:41:50 +0300

    Add files via upload

---
+++ CV/myScript.js
@@ -0,0 +1,17 @@
+
+function contactFunction()
+{   var content=document.getElementsByClassName("details")[0];
+    content.innerHTML="<div class=contactDiv><p class=jsclass>Phone Number: 0753513418 <br>E-mail:duminica.alex90@yahoo.com</p> </div>"
+}
+
+function HardFunction()
+{
+    var content=document.getElementsByClassName("details")[0];
+    content.innerHTML="<div class=hardDiv><ul class=jslist><li class=jslist>C++</li><li class=jslist>Python</li><li class=jslist>Assembly x86</li><li class=jslist>QT</li><li class=jslist>T-SQL</li><li class=jslist>Bash Scripting</li></ul></div>";
+}
+
+function SoftFunction()
+{
+    var content=document.getElementsByClassName("details")[0];
+    content.innerHTML="<div class=hardDiv><ul class=jslist><li class=jslist>Communication</li><li class=jslist>Hardworker</li><li class=jslist>Problem Solving</li><li class=jslist>Teamwork</li><li class=jslist>Very good time management</li><li class=jslist>Continue updating</li></ul></div>";
+}
